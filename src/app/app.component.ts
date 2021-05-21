dcff332334f23c39f457aa6af0a37e9f6120585a
GuzelGokhan <gokhan@medispring.be>
2021-05-21 16:51:06 +0200

    correction for search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,20 @@ import { Client } from './client.model';
 import { Account } from './account.model';
 import { DataService } from './data.service';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent implements OnInit {
   [x: string]: any;
   clients$: Client[];
   accounts$: Account[];
-  searchClient: string ; 
-  constructor(private dataService: DataService) { };
+  searchClient: string;
+  constructor(private dataService: DataService) {}
 
   ngOnInit() {
-    this.dataService.getClients()
-      .subscribe(data => this.clients$ = data);
-    this.dataService.getAccounts()
-      .subscribe(data => this.accounts$ = data);
+    this.dataService.getClients().subscribe((data) => (this.clients$ = data));
+    this.dataService.getAccounts().subscribe((data) => (this.accounts$ = data));
   }
 }
